d65680605ea7a56730471e951fc3b9da6e058e01
Martinelli-Vitor <vitormartinellilobo@gmail.com>
2021-07-10 21:29:03 -0300

    form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
+import { FormComponent } from './form/form.component';
+
+import { FotoComponent } from './foto/foto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FotoComponent,
+    FormComponent,
     AuthComponent
   ],
   imports: [
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'gina-form',
+    templateUrl: 'form.component.html',
+    
+})
+export class FormComponent {
+    
+}
