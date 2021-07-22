e20a795bd8bf5c26a633ad9b983c24851db62f1f
ilnen <lenin.ferreira@gmail.com>
2021-07-22 14:36:41 -0300

    Type: Merge
    Date: 22-07-2021
    
    Message:
    Preparaçao para merge com branch do vitor

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) { }
+
+  isLoggedIn() {
+    this.auth.onAuthStateChanged((user) => {
+      if (user)
+        return true
+      else
+        return false
+    })
+  }
 }
